0d28145b151c98f3a6652d1f61eaf384ccee7d56
Dobromir Ivanov <dobromirivanov1@gmail.com>
2025-04-14 18:53:29 +0300

    implement EventProducer.cs

---
+++ CQRS-ES/CQRS.Core/Producers/IEventProducer.cs
@@ -0,0 +1,10 @@
+using System.Threading.Tasks;
+using CQRS.Core.Events;
+
+namespace CQRS.Core.Producers
+{
+    public interface IEventProducer
+    {
+        Task ProduceAsync<T>(string topic, T @event) where T : BaseEvent;
+    }
+}
